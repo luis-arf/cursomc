18bb731b2fe0d0ac287a7e3e06e5aa282f87c410
Luis Arf <lu_arf@yahoo.com.br>
2021-12-24 08:42:22 -0300

    Banco de dados H2

--- src/main/java/com/luisarf/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luisarf/cursomc/resources/CategoriaResource.java
@@ -14,7 +14,7 @@ import com.luisarf.cursomc.domain.Categoria;
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public List listar() {
+	public List<Categoria> listar() {
 		
 		Categoria cat1 = new Categoria(1, "Informatica");
 		Categoria cat2 = new Categoria(2, "Escritorio");
